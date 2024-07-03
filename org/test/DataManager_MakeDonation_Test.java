e89ec235aa8c00011d4bc04488cc26631f312306
ijokoye <144378929+ijokoye@users.noreply.github.com>
2024-07-02 20:10:35 -0400

    added test for makedonation organization level

---
+++ org/test/DataManager_MakeDonation_Test.java
@@ -0,0 +1,126 @@
+
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class DataManager_MakeDonation_Test {
+
+
+
+    @Test
+    public void successfulDonation() {
+        DataManager dm = new DataManager(new WebClient("localhost", 3001) {
+
+            @Override
+            public String makeRequest(String resource, Map<String, Object> queryParams) {
+                if (resource.equals("/makeDonation")) {
+                    return "{\"status\":\"success\",\"message\":\"Donation successful\"}";
+                }
+                return null;
+            }
+        });
+
+        assertTrue(dm.makeDonation("6672ed0f926b0b73131c05f6", "66664ba1f39985059eb2", "200"));
+    }
+
+    @Test
+    public void invalidContributor() {
+        DataManager dm = new DataManager(new WebClient("localhost", 3001) {
+
+            @Override
+            public String makeRequest(String resource, Map<String, Object> queryParams) {
+                if (resource.equals("/makeDonation")) {
+                    return "{\"status\":\"error\",\"message\":\"Invalid contributor ID\"}";
+                }
+                return null;
+            }
+        });
+
+        assertFalse(dm.makeDonation("invalidContributor", "66664ba1f39985059eb2", "200"));
+    }
+
+
+
+
+    @Test
+    public void nonNumericAmount() {
+        DataManager dm = new DataManager(new WebClient("localhost", 3001) {
+            @Override
+            public String makeRequest(String resource, Map<String, Object> queryParams) {
+                if (resource.equals("/makeDonation")) {
+                    return "{\"status\":\"error\",\"message\":\"Invalid amount\"}";
+                }
+                return null;
+            }
+        });
+
+
+        dm.makeDonation("6672ed0f926b0b73131c05f6", "66664ba1f39985059eb2", "abc");
+
+    }
+
+    @Test
+    public void negativeAmount() {
+        DataManager dm = new DataManager(new WebClient("localhost", 3001) {
+            @Override
+            public String makeRequest(String resource, Map<String, Object> queryParams) {
+                if (resource.equals("/makeDonation")) {
+                    return "{\"status\":\"error\",\"message\":\"Negative amount\"}";
+                }
+                return null;
+            }
+        });
+
+        assertFalse(dm.makeDonation("6672ed0f926b0b73131c05f6", "66664ba1f39985059eb2", "-100"));
+    }
+
+    @Test
+    public void serverError() {
+        DataManager dm = new DataManager(new WebClient("localhost", 3001) {
+            @Override
+            public String makeRequest(String resource, Map<String, Object> queryParams) {
+                if (resource.equals("/makeDonation")) {
+                    return "{\"status\":\"error\",\"message\":\"Server error\"}";
+                }
+                return null;
+            }
+        });
+
+        assertFalse(dm.makeDonation("6672ed0f926b0b73131c05f6", "66664ba1f39985059eb2", "200"));
+    }
+
+
+//    @Test
+//    public void successfulChange() {
+//
+//
+//        DataManager dm = new DataManager(new WebClient("localhost", 3001) {
+//
+//
+//            @Override
+//            public String makeRequest(String resource, Map<String, Object> queryParams) {
+//                if (resource.equals("/makeDonation")) {
+//                    return "{\"status\":\"success\",\"data\":\"name\"}";
+//                }
+//                return null;
+//            }
+//
+//
+//        });
+//
+//
+////        assert(dm.changeOrgInfo("Name", "Description", "Id"));
+//        assert(dm.makeDonation("6672ed0f926b0b73131c05f6", "66664ba1f39985059eb2", "200"));
+//
+//
+//    }
+}
+
